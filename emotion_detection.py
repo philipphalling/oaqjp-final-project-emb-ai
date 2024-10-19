205c6c99244a4b5e4f522a357b95a3c15f269bd0
philipphalling <philipp.halling@gmail.com>
2024-10-19 15:30:10 -0400

    third done

--- emotion_detection.py
+++ emotion_detection.py
@@ -10,21 +10,15 @@ def emotion_detector(text_to_analyse):
         res.raise_for_status() 
         data = res.json()
 
-        scores = data.emotionPredictions.emotion
-        anger_score = scores.anger
-        disgust_score = scores.disgust
-        fear_score = scores.fear
-        joy_score = scores.joy
-        sadness_score = scores.sadness
-        dominant_emotion = "HAHA!"
+        scores = data['emotionPredictions'][0]['emotion']
 
         apiReturn = {
-            'anger': anger_score,
-            'disgust': disgust_score,
-            'fear': fear_score,
-            'joy': joy_score,
-            'sadness': sadness_score,
-            'dominant_emotion': dominant_emotion,
+            'anger': scores['anger'],
+            'disgust': scores['disgust'],
+            'fear': scores['fear'],
+            'joy': scores['joy'],
+            'sadness': scores['sadness'],
+            'dominant_emotion': max(scores, key=scores.get),
         }
         return apiReturn
     except requests.exceptions.RequestException as e:

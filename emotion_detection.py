eace3b3e81231441b5ce57d0fc954c36371b9369
philipphalling <philipp.halling@gmail.com>
2024-10-19 05:46:24 -0400

    task3 ongoing

--- emotion_detection.py
+++ emotion_detection.py
@@ -9,7 +9,24 @@ def emotion_detector(text_to_analyse):
         res = requests.post(url=URL, json=OBJ, headers=HEADERS)
         res.raise_for_status() 
         data = res.json()
-        return data
+
+        scores = data.emotionPredictions.emotion
+        anger_score = scores.anger
+        disgust_score = scores.disgust
+        fear_score = scores.fear
+        joy_score = scores.joy
+        sadness_score = scores.sadness
+        dominant_emotion = "HAHA!"
+
+        apiReturn = {
+            'anger': anger_score,
+            'disgust': disgust_score,
+            'fear': fear_score,
+            'joy': joy_score,
+            'sadness': sadness_score,
+            'dominant_emotion': dominant_emotion,
+        }
+        return apiReturn
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
         return None
